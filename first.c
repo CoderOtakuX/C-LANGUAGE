e494617452ed35341e0cfb9d7b59953227e916cf
Arjun_Negi <156993385+CoderOtakuX@users.noreply.github.com>
2024-03-24 22:30:02 +0530

    Update first.c

--- first.c
+++ first.c
@@ -5,8 +5,19 @@ int main (){
     printf("HELLO WORLD");
 } */
 
-PROGRAM TO CHECK WHETHER THE NUMBER IS ODD OR EVEN  
+/*PROGRAM TO CHECK WHETHER THE NUMBER IS ODD OR EVEN  
 
+#include <stdio.h>
+int main() {
+    int no;
+    printf("ENTER ANY NUMBER");
+    scanf("%d", &no);
+    if (no % 2 == 0) {
+        printf("%d is EVEN", no);
+    } else {
+        printf("%d is ODD", no);
+    }
+} */
 
 
 
