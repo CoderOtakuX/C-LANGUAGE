4271b1b19167518b35554c4dcfcaefde27810efd
Arjun_Negi <156993385+CoderOtakuX@users.noreply.github.com>
2024-03-25 22:38:17 +0530

    Update first.c

--- first.c
+++ first.c
@@ -19,7 +19,27 @@ int main() {
     }
 } */
 
-// PROGRAM TO FIND THE LARGEST OF THE THREE NUMBER 
+/*PROGRAM TO FIND THE LARGEST OF THE THREE NUMBER 
 
+ #include <stdio.h>
+
+int main() {
+    int num1, num2, num3;
+
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+
+    if (num1 >= num2 && num1 >= num3) {
+        printf("%d is the largest number.", num1);
+    }
+    else if (num2 >= num1 && num2 >= num3) {
+        printf("%d is the largest number.", num2);
+    }
+    else {
+        printf("%d is the largest number.", num3);
+    }
+
+    return 0;
+} */
 
 
