472f3c27b0dc5526efdf58b87eca4d6ae6234e13
Arjun_Negi <156993385+CoderOtakuX@users.noreply.github.com>
2024-04-06 22:29:45 +0530

    Update second.c

--- second.c
+++ second.c
@@ -1,1 +1,12 @@
  1. WAP TO PRINT 1 TO 10 USING WHILE LOOP 
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+    
+    while (i <= 10) {
+        printf("%d \n", i);
+        i++;
+    }
+    
+    return 0;
