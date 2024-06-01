34fbcf5ca071103b194e34e62d29f5e421e15ea2
Arjun_Negi <156993385+CoderOtakuX@users.noreply.github.com>
2024-06-01 22:36:13 +0530

    Update first.c

--- first.c
+++ first.c
@@ -1,9 +1,9 @@
-/* PROGRAM TO PRINT HELLO WORLD
+./* PROGRAM TO PRINT HELLO WORLD
 
 #include <stdio.h> 
 int main (){.
     printf("HELLO WORLD");
-} */ 
+} */ .
 
 /*PROGRAM TO CHECK WHETHER THE NUMBER IS ODD OR EVEN  
 
